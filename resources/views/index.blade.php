6b3a2418af9bf25288ca2c2054a9e00d7d336998
ShodanMaster <akshaykrishnan922@gmail.com>
2025-10-25 21:57:15 +0530

    Index Setup

---
+++ resources/views/index.blade.php
@@ -0,0 +1,4 @@
+@extends('master')
+@section('content')
+    <h1 class="text-center">Welcome to Polymorphic Relationship Example</h1>
+@endsection
---
+++ resources/views/master.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Polymorphic Relationship</title>
+    <link rel="stylesheet" href="{{ asset('asset/bootstrap.min.css') }}">
+
+</head>
+<body>
+
+    <div class="container mt-5">
+        @yield('content')
+    </div>
+
+    <script src="{{ asset('asset/jquery.min.js') }}"></script>
+    <script src="{{ asset('asset/bootstrap.min.js') }}"></script>
+</body>
+</html>
